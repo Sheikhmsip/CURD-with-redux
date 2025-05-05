936948f1615b085af0fb6dcec4e112b1ace8d929
Sheikhmsip <sheikhmsip@gmail.com>
2025-05-05 16:10:51 +0600

    Setup redux store with RTK query api slice and ref with provider

---
+++ src/features/api/apiSlice.js
@@ -0,0 +1,45 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:5000/",
+  }),
+  tagTypes: ["Posts"],
+  endpoints: (builder) => ({
+    getPosts: builder.query({
+      query: () => "/posts",
+      providesTags: ["Posts"],
+    }),
+    addPost: builder.mutation({
+      query: (newPost) => ({
+        url: "/posts",
+        method: "POST",
+        body: newPost,
+      }),
+      invalidatesTags: ["Posts"],
+    }),
+    updatePost: builder.mutation({
+      query: ({ id, ...patch }) => ({
+        url: `/posts/${id}`,
+        method: "PUT",
+        body: patch,
+      }),
+      invalidatesTags: ["Posts"],
+    }),
+    deletePost: builder.mutation({
+      query: (id) => ({
+        url: `/posts/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Posts"],
+    }),
+  }),
+});
+
+export const {
+  useGetPostsQuery,
+  useAddPostMutation,
+  useUpdatePostMutation,
+  useDeletePostMutation,
+} = apiSlice;
